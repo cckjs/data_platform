1eff105116619c1ad566f578e2c0434105a29b3b
yangyong <39901650@qq.com>
2015-05-15 15:46:41 +0800

    例子

---
+++ example/com/yy/data/platform/cms/bean/User.java
@@ -0,0 +1,56 @@
+package com.yy.data.platform.cms.bean;
+
+public class User {
+
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String gender;
+	
+	private String registerTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(String registerTime) {
+		this.registerTime = registerTime;
+	}
+	
+	
+}
